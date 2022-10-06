1cb9a4191724c4adfa6c1724da70b4d2465101f8
sy8044 <43639693+sy8044@users.noreply.github.com>
2022-10-06 23:06:02 +0900

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -22,7 +22,7 @@ def dijkstra(start) :
         for i in graph[now] :
             cost = dist + i[1]
             if cost > distance[i[0]] :
-                cost = distance[i[0]]
+                distance[i[0]] = cost
                 heapq.heappush(q,(cost,i[0]))
 
 dijkstra(start)
